5170dee89e41fbbe0a87bc263fd4cd935ef15dfa
Damon <853669721@qq.com>
2025-03-25 00:45:50 +0800

    1. 完整解决找到旋转数组中的最小数字问题 2. 测试一下添加了修改人信息
    Signed-off-by: Damon <853669721@qq.com>

--- src/11_find_min_in_spin_vec.cpp
+++ src/11_find_min_in_spin_vec.cpp
@@ -11,12 +11,15 @@ int FindMinInSpinVec(std::vector<int>& vec)
     int right = vec.size() - 1;
     int mid;
     while (left < right) {
+        if (right - left == 1) { return vec[right]; }
         mid = (right - left) / 2 + left;
-        if (vec[mid] < vec[right]) {
-            left = mid + 1;
-        } else if (vec[mid] < vec[right]) {
-            right = mid - 1;
+        // 不能使用mid和左右两边比较是否小，因为可能存在当前值是最大值的可能。
+        // 理想状态应该是，最后状态是，左边指针是最大值，右边指针是最小值。
+        if (vec[mid] >= vec[left]) {    // 这里一定要包含等于，可能出现3112 这样的序列，如此还是可能死循环
+            left = mid;
+        } else if (vec[mid] <= vec[right]) {
+            right = mid;
         }
     }
-    return vec[mid];
+    return vec[left];
 }
--- tests/11_find_min_in_spin_vec_tests.cpp
+++ tests/11_find_min_in_spin_vec_tests.cpp
@@ -5,8 +5,38 @@
 #include "my_algorithms.h"
 #include <gtest/gtest.h>
 
-TEST(FindMinInSpinVecTest, Test1)
+TEST(FindMinInSpinVecTest, EmptyTest)
+{
+    std::vector<int> vec = {};
+    EXPECT_EQ(FindMinInSpinVec(vec), -1);
+}
+
+TEST(FindMinInSpinVecTest, OneElement)
+{
+    std::vector<int> vec = {0};
+    EXPECT_EQ(FindMinInSpinVec(vec), 0);
+}
+
+TEST(FindMinInSpinVecTest, AllSameElement)
+{
+    std::vector<int> vec = {1, 1, 1, 1, 1, 1};
+    EXPECT_EQ(FindMinInSpinVec(vec), 1);
+}
+
+TEST(FindMinInSpinVecTest, NormalTest)
 {
     std::vector<int> vec = {4, 5, 6, 7, 1, 2, 3};
+    EXPECT_EQ(FindMinInSpinVec(vec), 1);
+}
+
+TEST(FindMinInSpinVecTest, NormalTest2)
+{
+    std::vector<int> vec = {4, 5, 6, 7, 1, 2, 3};
+    EXPECT_EQ(FindMinInSpinVec(vec), 1);
+}
+
+TEST(FindMinInSpinVecTest, NormalTest3)
+{
+    std::vector<int> vec = {4, 5, 6, 7, 8, 1, 1, 2, 3};
     EXPECT_EQ(FindMinInSpinVec(vec), 1);
 }
