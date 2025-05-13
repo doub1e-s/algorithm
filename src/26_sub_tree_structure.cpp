1fc3ffc7141cb77173b544872c3f42765d1dfdb5
Damon <853669721@qq.com>
2025-05-14 01:14:19 +0800

    添加少量注释，在node2为空的场景，交给subTree处理
    Signed-off-by: Damon <853669721@qq.com>

--- src/26_sub_tree_structure.cpp
+++ src/26_sub_tree_structure.cpp
@@ -1,5 +1,6 @@
 #include "my_algorithms.h"
 
+// 设计目标是确认是否为同一个树，其中一个为空也直接返回false
 bool IsSameTree(TreeNode* node1, TreeNode* node2)
 {
     if (!node1 && !node2) { return true; }
@@ -8,8 +9,10 @@ bool IsSameTree(TreeNode* node1, TreeNode* node2)
     return IsSameTree(node1->m_pLeft, node2->m_pLeft) && IsSameTree(node1->m_pRight, node2->m_pRight);
 }
 
+// 设计目标是确认是否为子树,只要node2是空，就返回false，但是要优先判断node2
 bool IsSubTree(TreeNode* node1, TreeNode* node2)
 {
+    // 处理node2为空的场景
     if (!node2) { return true; }
     if (!node1) { return false; }
     return IsSameTree(node1, node2) || IsSubTree(node1->m_pLeft, node2) || IsSubTree(node1->m_pRight, node2);
