fabc6bbc094d8ab88129f875e521d8db1876be0b
Damon <853669721@qq.com>
2025-06-15 10:16:25 +0800

    二次手写
    Signed-off-by: Damon <853669721@qq.com>

--- src/53_find_n_cnt_in_order_arr.cpp
+++ src/53_find_n_cnt_in_order_arr.cpp
@@ -14,10 +14,10 @@ int FindFirstNum(const vector<int>& arr, int target)
         if (arr[mid] == target) {
             firstInd = mid;
             right = mid - 1;
-        } else if (arr[mid] > target) {
-            right = mid - 1;
-        } else {
+        } else if (arr[mid] < target) {
             left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
     return firstInd;
@@ -28,17 +28,19 @@ int FindLastNum(const vector<int>& arr, int target)
     int left = 0;
     int right = arr.size() - 1;
     int lastInd = 0;
+
     while (left <= right) {
         int mid = left + ((right - left) >> 1);
         if (arr[mid] == target) {
             lastInd = mid;
             left = mid + 1;
-        } else if (arr[mid] > target) {
-            right = mid - 1;
-        } else {
+        } else if (arr[mid] < target) {
             left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
     return lastInd;
 }
 
