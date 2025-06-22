7dd57b654ee732c0f05fbd76ed0280ebfb2ce64f
Damon <853669721@qq.com>
2025-06-22 11:29:20 +0800

    重新梳理一下，然后手写快排。核心是当前是与right进行对比，所以最后如果有比当前值更大的值，将其放到right
    Signed-off-by: Damon <853669721@qq.com>

--- src/00_quic_sort.cpp
+++ src/00_quic_sort.cpp
@@ -6,34 +6,33 @@
 
 int Partition(vector<int>& vec, int left, int right)
 {
+    if (left >= right) { return left; }
     int small = left;
     int big = right - 1;
+    int pivot = vec[right];
     while (small < big) {
-        while (small < big && vec[small] <= vec[right]) {
+        while (small < big && vec[small] <= pivot) {
             small++;
         }
-        while (small < big && vec[big] >= vec[right]) {
+        while (small < big && vec[big] >= pivot) {
             big--;
         }
         swap(vec[small], vec[big]);
     }
-    // 左侧全部都是小于right的情况，直接返回当前的index
-    if (small == right - 1 && vec[small] <= vec[right]) {
-        return right;
+    // 如果存在比vec[right]更大的值，将他和right调换位置
+    if (vec[small] >= pivot) {
+        swap(vec[right], vec[small]);
     }
-    // else small和big相当，或者right是
-    swap(vec[right], vec[big]);
-    return big;
+    return small;
 }
 
 void QuickSort(vector<int>& vec, int left, int right)
 {
-    if (vec.empty() || left < 0 || right >= vec.size() || left >= right) {
-        return ;
-    }
-    // 1. 直接选择数组中最后一个数字，以这个数字为参考对象， 小于的放在左边，大于的放在右边
-    int index = Partition(vec, left, right);   
-    // 2. 分别递归执行左边剩余数字和右边剩余数字
+    // 如果vec为空，或者left/right超过边界，直接返回
+    if (vec.empty() || left < 0 || right >= vec.size() || left >= right) { return ; }
+    // 找一个数字，将数组中的所有大于他的都放左边，小于他的都放右边
+    int index = Partition(vec, left, right);
+    // 返回这个数字的下标，递归执行他的左侧和右侧数组
     QuickSort(vec, left, index - 1);
     QuickSort(vec, index + 1, right);
 }
