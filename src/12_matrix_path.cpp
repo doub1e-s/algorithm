47c6015c311a1ba9ef33d6707d5b6b782245f89e
Damon <853669721@qq.com>
2025-06-30 08:22:00 +0800

    重新写一遍矩阵中的路径

--- src/12_matrix_path.cpp
+++ src/12_matrix_path.cpp
@@ -4,23 +4,30 @@
 
 #include "my_algorithms.h"
 
-bool FindExistPath(const std::vector<std::vector<char>>& matrix, int row, int col,
-    std::vector<std::vector<bool>>& visited, std::string target, int index)
+bool FindExistPathHelper(const vector<std::vector<char>>& matrix, int row, int col, vector<vector<bool>>& visited,
+    const string target, int index)
 {
-    if (index == target.size()) { return true; }
-    // 超过边界
-    if (row < 0 || row >= matrix.size() || col < 0 || col >= matrix[0].size()) { return false; }
-    // 已经访问过或者和target目标不同
-    if (matrix[row][col] != target[index] || visited[row][col]) { return false; }
+    // 当前char是否有必要检验
+    if (row < 0 || row >= matrix.size() || col < 0 || col >= matrix[0].size() || visited[col][row]) {
+        return false;
+    }
+    // 判断当前的char和目标char是否相同
+    if (matrix[row][col] != target[index]) {
+        return false;
+    }
+    if (index == target.size() - 1) {   // 找到了匹配的字符
+        return true;
+    }
     visited[row][col] = true;
-    bool exists = FindExistPath(matrix, row - 1, col, visited, target, index + 1) ||
-        FindExistPath(matrix, row + 1, col, visited, target, index + 1) ||
-        FindExistPath(matrix, row, col - 1, visited, target, index + 1) ||
-        FindExistPath(matrix, row, col + 1, visited, target, index + 1);
-    visited[row][col] = false;
-    return exists;
+    bool res = FindExistPathHelper(matrix, row + 1, col, visited, target, index + 1) ||
+        FindExistPathHelper(matrix, row - 1, col, visited, target, index + 1) ||
+        FindExistPathHelper(matrix, row, col + 1, visited, target, index + 1) ||
+        FindExistPathHelper(matrix, row, col - 1, visited, target, index + 1);
+    visited[row][col] = false;  // 回溯
+    return res;
 }
 
+// 矩阵中的路径
 bool FindExistPath(const std::vector<std::vector<char>>& matrix, const std::string& target)
 {
     if (matrix.empty() || matrix[0].empty()) { return false; }
@@ -29,8 +36,8 @@ bool FindExistPath(const std::vector<std::vector<char>>& matrix, const std::stri
     for (int row = 0; row < matrix.size(); row++) {
         for (int col = 0; col < matrix[0].size(); col++) {
             // 只有和第一个字符相同才继续判断
-            if (matrix[row][col] == target[0]) {
-                if (FindExistPath(matrix, row, col, visited, target, 0)) {
+            if (matrix[row][col] == target[0]) {    // 起点可以是任何一个点
+                if (FindExistPathHelper(matrix, row, col, visited, target, 0)) {
                     return true;
                 }
             }
