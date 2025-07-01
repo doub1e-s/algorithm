a101c240b6a43643d7f5c74314e58960b16bb137
Damon <853669721@qq.com>
2025-07-01 10:52:58 +0800

    机器人在M*N的棋盘中所有能否飞到的格子数量。

--- src/13_robot_move.cpp
+++ src/13_robot_move.cpp
@@ -19,25 +19,29 @@ int GetSeprateSum(int num)
     return ret;
 }
 
-std::unordered_map<int, std::unordered_set<int>> visitedIndex;
+ std::unordered_map<int, std::unordered_set<int>> visitedIndex;  // 如果不限制在一个m*n的棋盘中，就应该使用这种容器的形式
 }
 
-int RobotMoveRange(int row, int col, int rowBound, int colBound, int k)
+int RobotMoveRangeHelper(int row, int col, int rowBound, int colBound, int k, vector<vector<bool>>& visited)
 {
-    int rowSum = GetSeprateSum(row);
-    int colSum = GetSeprateSum(col);
-    // 可以考虑将上面两个变量放到边界值判定的后面，可能会减少执行的次数，降低cpu的消耗
-    if (rowSum + colSum > k || row > rowBound || col > colBound ||
-        (visitedIndex.find(row) != visitedIndex.end() && visitedIndex[row].find(col) != visitedIndex[row].end())) {
+    if (row > rowBound || row < 0 || col < 0 || col > colBound ||
+        GetSeprateSum(row) + GetSeprateSum(col) > k || visited[row][col]) {
         return 0;
     }
-    visitedIndex[row].insert(col);
-    return 1 + RobotMoveRange(row+1, col, rowBound, colBound, k) + RobotMoveRange(row, col+1, rowBound, colBound, k);
+    visited[row][col] = true;
+    return 1 + RobotMoveRangeHelper(row + 1, col, rowBound, colBound, k, visited) +
+        RobotMoveRangeHelper(row - 1, col, rowBound, colBound, k, visited) +
+        RobotMoveRangeHelper(row, col - 1, rowBound, colBound, k, visited) +
+        RobotMoveRangeHelper(row, col + 1, rowBound, colBound, k, visited);
 }
 
+/*
+ *  机器人只能走按位相加的和的长度的范围请问他能走多少个格子,例如，12,34，最终的k值和为10.
+ *  20虽然相加等于2，但是他需要从19走过去，不能闪烁，所以应该通过递归的形式去计算所有能访问的总数
+ */
 int RobotMoveRange(int rowBound, int colBound, int k)
 {
-    if (k == 0) { return 0; }
-    visitedIndex.clear();
-    return RobotMoveRange(0, 0, rowBound, colBound, k);
+    if (k == 0 || rowBound <= 0 || colBound<= 0) { return 0; }
+    vector<vector<bool>> visited(rowBound + 1, vector<bool>(colBound + 1, false));
+    return RobotMoveRangeHelper(0, 0, rowBound, colBound, k, visited);
 }
