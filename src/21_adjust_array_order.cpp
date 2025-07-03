f1a31cdd6f9e33713b23643bd2b808e19a27e9e0
Damon <853669721@qq.com>
2025-07-03 23:14:14 +0800

    重写数组重排序，使用传递函数指针的形式优化写法

--- src/21_adjust_array_order.cpp
+++ src/21_adjust_array_order.cpp
@@ -8,17 +8,15 @@
 // 需要考虑扩展性，当前题目是让所有的奇数放到左边，所有的偶数放到右边
 void AdjustArrayOrder(std::vector<int>& vec, Predict predict)
 {
+    if (vec.empty()) { return ; }
     int left = 0, right = vec.size() - 1;
     while (left < right) {
-        // 左指针所有的奇数都跳过
         while (left < right && predict(vec[left])) {
             left++;
         }
         while (left < right && !predict(vec[right])) {
             right--;
         }
-        if (left < right) {
-            std::swap(vec[left], vec[right]);
-        }
+        swap(vec[left], vec[right]);
     }
 }
