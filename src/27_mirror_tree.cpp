f3a6dc027a24dbe1da70b8e4a203f718aafb24c5
Damon <853669721@qq.com>
2025-07-07 23:47:31 +0800

    简单提交

--- src/27_mirror_tree.cpp
+++ src/27_mirror_tree.cpp
@@ -9,7 +9,7 @@ TreeNode* MirrorTreeNode(TreeNode* head)
     if (!head) { return nullptr; }
     TreeNode* tempRight = head->right;
     TreeNode* tempLeft = head->left;
-    head->left = MirrorTreeNode(tempRight);
+    head->left = MirrorTreeNode(tempRight); // 使用临时指针，确保不会使用成赋值后的值
     head->right = MirrorTreeNode(tempLeft);
     return head;
 }
